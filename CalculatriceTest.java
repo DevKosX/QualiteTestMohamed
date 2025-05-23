cbc303b2d08667a4eb524e1a054272611cfef56e
DevKosX <kosbarmohamed.31@example.com>
2025-05-23 10:55:17 +0200

    Premier test initialisation pour les additions

---
+++ CalculatriceTest.java
@@ -0,0 +1,22 @@
+package com.example.demo.data;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CalculatriceTest {
+    private Calculatrice calculatrice;
+
+    @BeforeEach
+    void init(){
+        calculatrice = new Calculatrice();
+    }
+
+    @Test
+    void TestAddition(){
+        assertEquals(5, calculatrice.add(1.0, 4.0));
+        assertEquals(10, calculatrice.add(5.0, 5.0))
+    }
+}
